ac9c65c23c33505320267585399f53c23ebad9af
abattais <alexandre-bat@hotmail.fr>
2025-06-12 22:17:32 +0000

    Env working with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./src/**/*.{astro,js,jsx,ts,tsx}", "./public/admin/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
